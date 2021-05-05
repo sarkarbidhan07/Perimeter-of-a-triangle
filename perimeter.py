0b3b8f4221096d5dc04fccc222846400d74a7838
sarkarbidhan07 <57905138+sarkarbidhan07@users.noreply.github.com>
2021-05-05 09:57:18 +0600

    Add files via upload

---
+++ perimeter.py
@@ -0,0 +1,9 @@
+def main():
+    a=float(input("Length of side1:" ))
+    b=float(input("Base  of side:" ))
+    c=float(input("Length of side2:" ))
+    peri=a+b+c
+    print("Perimeter of this triangle is" ,peri)
+
+if __name__ == "__main__":
+    main()
